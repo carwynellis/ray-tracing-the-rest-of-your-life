5ffe12a57a5a3581b5c35b323e6644d11f737383
Carwyn Ellis <carwynellis@gmail.com>
2019-05-19 17:06:39 +0100

    Added jitter based Pi estimation

--- src/main/scala/uk/carwynellis/raytracing/montecarlo/Pi.scala
+++ src/main/scala/uk/carwynellis/raytracing/montecarlo/Pi.scala
@@ -1,5 +1,7 @@
 package uk.carwynellis.raytracing.montecarlo
 
+import scala.collection.immutable
+
 object Pi {
 
   /**
@@ -31,6 +33,34 @@ object Pi {
     }
 
     (4.0 * hitsInsideCircle) / iterations
+  }
+
+  /**
+    * Compute an estimate of Pi by computing a random point within each square of a grid.
+    *
+    * This approach is known as jittering and yields a more accurate result over fewer iterations.
+    *
+    * @param gridSize
+    * @return
+    */
+  def stratifiedEstimate(gridSize: Int): Double = {
+
+    def component(n: Int): Double = 2 * ((n + math.random()) / gridSize) - 1
+
+    val hits = for {
+      i <- 0 to gridSize
+      j <- 0 to gridSize
+    } yield {
+      val x = component(i)
+      val y = component(j)
+
+      // Determine if point (x,y) is inside the circle or not.
+      ((x * x) + (y * y)) < 1
+    }
+
+    val hitsInsideCircle = hits.count(h => h)
+
+    (4.0 * hitsInsideCircle) / (gridSize * gridSize)
   }
 
 }
--- src/test/scala/uk/carwynellis/raytracing/montecarlo/PiSpec.scala
+++ src/test/scala/uk/carwynellis/raytracing/montecarlo/PiSpec.scala
@@ -18,4 +18,10 @@ class PiSpec extends FlatSpec with Matchers {
     Pi.simpleEstimate(1000000) shouldBe 3.141 +- 0.01
   }
 
+  behavior of "stratifiedEstimate"
+
+  it should "compute an estimate of Pi to 5 decimal places with a grid size of 10000 x 10000" in {
+    Pi.stratifiedEstimate(10000) shouldBe 3.14159 +- 0.00001
+  }
+
 }
