a96cec4976ac5b0c534073421d643864a61e9b65
Carwyn Ellis <carwynellis@gmail.com>
2019-06-22 18:14:00 +0100

    Test revisions following recent API changes.

--- src/test/scala/uk/carwynellis/raytracing/RendererTest.scala
+++ src/test/scala/uk/carwynellis/raytracing/RendererTest.scala
@@ -15,41 +15,47 @@ class RendererTest extends FunSuite with Matchers {
     val height = 400
     val samples = 4
 
-    val origin = Vec3(278, 278, -800)
-    val target = Vec3(278, 278, 0)
 
-    val time0 = 0.0
-    val time1 = 0.2
 
-    val camera = Camera(
-      origin = origin,
-      target = target,
-      upVector = Vec3(0, 1, 0),
-      verticalFieldOfView = 40,
-      aspectRatio =  width.toDouble / height.toDouble,
-      aperture = 0.0,
-      focusDistance = 10,
-      time0 = time0,
-      time1 = time1
-    )
+    val failingScene = {
+      val origin = Vec3(278, 278, -800)
+      val target = Vec3(278, 278, 0)
+      val time0 = 0.0
+      val time1 = 0.2
+      // Replicate scene generating bad values.
+      val red = Lambertian(ConstantTexture(Vec3(0.65, 0.05, 0.05)))
+      val light = DiffuseLight(ConstantTexture(Vec3(15, 15, 15)))
+      val lightSource = XZRectangle(213, 343, 227, 332, 554, light)
 
-    // Replicate scene generating bad values.
-    val red = Lambertian(ConstantTexture(Vec3(0.65, 0.05, 0.05)))
-    val light = DiffuseLight(ConstantTexture(Vec3(15, 15, 15)))
+      val objects = HitableList(List(
+        YZRectangle(0, 555, 0, 555, 0, red), // Right wall
+        lightSource
+      ))
 
-    val failingScene = HitableList(List(
-      YZRectangle(0, 555, 0, 555, 0, red), // Right wall
-      XZRectangle(213, 343, 227, 332, 554, light),
-    ))
+      Scene(
+        objects = objects,
+        raySources = HitableList(List(lightSource)),
+        camera = Camera(
+          origin = origin,
+          target = target,
+          upVector = Vec3(0, 1, 0),
+          aspectRatio =  width.toDouble / height.toDouble,
+          time0 = time0,
+          time1 = time1
+        )
+      )
 
-    val renderer = Renderer(camera, failingScene, width, height, samples)
+    }
+
+
+    val renderer = Renderer(failingScene, width, height, samples)
 
     // This pixel is known to generate values out of bounds.
     val pixel = renderer.renderPixel(265, 347)
 
-    pixel.r should be < 256
-    pixel.g should be < 256
-    pixel.b should be < 256
+    pixel.r should be < 256.0
+    pixel.g should be < 256.0
+    pixel.b should be < 256.0
   }
 
 }
